f6aeba7584b00fc6363c46f1800229779399a926
Diego <diegogomezledo@gmail.com>
2020-03-04 21:31:49 +0100

    Cambio en el cursor y en las opciones

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ io.on('connection', socket => {
 
         socket.to(partidas[partidaAsignada].nombre).emit('user-disconnect', idJugador);
         socket.leave(partidas[partidaAsignada].nombre);
-        partidas[partidaAsignada].jugadores--;
+        if(partidas[partidaAsignada].jugadores!=3) partidas[partidaAsignada].jugadores--;
 
         if(partidas[partidaAsignada].jugadores<3 && partidas[partidaAsignada].playing) 
             partidas[partidaAsignada].playing = false;
