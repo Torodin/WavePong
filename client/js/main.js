37138224556dc83fb963f872332c083d2681bb13
Torodin <diegogomezledo@gmail.com>
2020-03-12 13:47:29 +0100

    Cambios de ventana

--- client/js/main.js
+++ client/js/main.js
@@ -29,7 +29,7 @@ const TEXT_COLOR = '#E900FF';
 
 // Caracteristicas pelota
 const PELOTA_RAD = 13;
-const PELOTA_POS_INI = [550,360];
+const PELOTA_POS_INI = [550, 360];
 const PELOTA_MIN_VEL = 5;
 const PELOTA_MAX_VEL = 10;
 const PELOTA_COLOR = '#F52789';
@@ -43,7 +43,7 @@ const PLANK_VEL = 3.6;
 const PLANK_COLOR = '#FAEB2C';
 
 // Los muros y porterias
-const PORTERIAS = [2,5,8];
+const PORTERIAS = [2, 5, 8];
 
 const WALLS = [
     [140, 310],
@@ -62,7 +62,7 @@ const PORT_COLOR = '#F52789';
 
 // Caracteristicas para la puntiacion
 let ultimoGolpe = -1;
-let puntuaciones = [0,0,0];
+let puntuaciones = [0, 0, 0];
 
 // create an engine
 let engine = Engine.create();
@@ -89,30 +89,30 @@ var render = CustomRender.create({
 engine.world.gravity.y = 0;
 
 // -1.05
-let angle = [-1.05,0,1.57,0,1.05,-0.52,1.05,-1.05,0.52];
+let angle = [-1.05, 0, 1.57, 0, 1.05, -0.52, 1.05, -1.05, 0.52];
 
 let wallsMedio = [];
-for (var i=0; i<WALLS.length; i++) {
-    let nextInd = (i==WALLS.length-1)? 0:i+1;
+for (var i = 0; i < WALLS.length; i++) {
+    let nextInd = (i == WALLS.length - 1) ? 0 : i + 1;
 
     wallsMedio.push([
-        (WALLS[i][0]+WALLS[nextInd][0])/2,
-        (WALLS[i][1]+WALLS[nextInd][1])/2,
+        (WALLS[i][0] + WALLS[nextInd][0]) / 2,
+        (WALLS[i][1] + WALLS[nextInd][1]) / 2,
     ]);
 }
 
 let wallsWorld = [];
 let wallJ = 0;
 
-for(var i=0; i<wallsMedio.length; i++) {
+for (var i = 0; i < wallsMedio.length; i++) {
     let tmp;
 
-    if(PORTERIAS.includes(i)) {
-        tmp = Bodies.rectangle(wallsMedio[i][0], wallsMedio[i][1], PLANK_HEGHT, PLANK_WIDTH, 
+    if (PORTERIAS.includes(i)) {
+        tmp = Bodies.rectangle(wallsMedio[i][0], wallsMedio[i][1], PLANK_HEGHT, PLANK_WIDTH,
             {
                 label: 'porteria',
                 jugador: wallJ,
-                isStatic:true,
+                isStatic: true,
                 chamfer: { radius: 3 },
                 angle: angle[i],
                 render: {
@@ -124,10 +124,10 @@ for(var i=0; i<wallsMedio.length; i++) {
         wallJ++;
 
     } else {
-        tmp = Bodies.rectangle(wallsMedio[i][0], wallsMedio[i][1], PLANK_HEGHT, PLANK_WIDTH, 
+        tmp = Bodies.rectangle(wallsMedio[i][0], wallsMedio[i][1], PLANK_HEGHT, PLANK_WIDTH,
             {
                 label: 'muro',
-                isStatic:true,
+                isStatic: true,
                 chamfer: { radius: 3 },
                 angle: angle[i],
                 render: {
@@ -142,7 +142,7 @@ for(var i=0; i<wallsMedio.length; i++) {
 
 World.add(engine.world, wallsWorld);
 
-var bola = Bodies.circle(PELOTA_POS_INI[0],PELOTA_POS_INI[1],PELOTA_RAD,
+var bola = Bodies.circle(PELOTA_POS_INI[0], PELOTA_POS_INI[1], PELOTA_RAD,
     {
         label: 'Pelota',
         inertia: 0,
@@ -157,7 +157,7 @@ var bola = Bodies.circle(PELOTA_POS_INI[0],PELOTA_POS_INI[1],PELOTA_RAD,
     }
 );
 
-var paleta = Bodies.rectangle(550, 710, 10, 75, 
+var paleta = Bodies.rectangle(550, 710, 10, 75,
     {
         label: 'paleta1',
         isStatic: true,
@@ -169,8 +169,8 @@ var paleta = Bodies.rectangle(550, 710, 10, 75,
     }
 );
 
-var posPal2 = moveAngle( Vector.create(885, 180), 53, -2.35 );
-var paleta2 = Bodies.rectangle(posPal2.x, posPal2.y, 10, 75, 
+var posPal2 = moveAngle(Vector.create(885, 180), 53, -2.35);
+var paleta2 = Bodies.rectangle(posPal2.x, posPal2.y, 10, 75,
     {
         label: 'paleta2',
         isStatic: true,
@@ -182,8 +182,8 @@ var paleta2 = Bodies.rectangle(posPal2.x, posPal2.y, 10, 75,
     }
 );
 
-var posPal3 = moveAngle( Vector.create(215, 220), 50, 1.35 );
-var paleta3 = Bodies.rectangle(posPal3.x, posPal3.y, 10, 75, 
+var posPal3 = moveAngle(Vector.create(215, 220), 50, 1.35);
+var paleta3 = Bodies.rectangle(posPal3.x, posPal3.y, 10, 75,
     {
         label: 'paleta3',
         isStatic: true,
@@ -197,7 +197,7 @@ var paleta3 = Bodies.rectangle(posPal3.x, posPal3.y, 10, 75,
 
 let paletas = [paleta, paleta2, paleta3];
 
-var puntuacionJ1 = Bodies.rectangle(180, 650, 1, 1, 
+var puntuacionJ1 = Bodies.rectangle(180, 650, 1, 1,
     {
         label: "cont1",
         isStatic: true,
@@ -213,7 +213,7 @@ var puntuacionJ1 = Bodies.rectangle(180, 650, 1, 1,
     }
 );
 
-var puntuacionJ2 = Bodies.rectangle(180, 680, 1, 1, 
+var puntuacionJ2 = Bodies.rectangle(180, 680, 1, 1,
     {
         label: "cont2",
         isStatic: true,
@@ -229,7 +229,7 @@ var puntuacionJ2 = Bodies.rectangle(180, 680, 1, 1,
     }
 );
 
-var puntuacionJ3 = Bodies.rectangle(180, 710, 1, 1, 
+var puntuacionJ3 = Bodies.rectangle(180, 710, 1, 1,
     {
         label: "cont3",
         isStatic: true,
@@ -247,15 +247,15 @@ var puntuacionJ3 = Bodies.rectangle(180, 710, 1, 1,
 
 Events.on(engine, 'collisionStart', event => {
     let pairs = event.pairs;
-    
+
     for (var i = 0, j = pairs.length; i != j; ++i) {
         let pair = pairs[i];
 
         // Si la pelota choca con una porteria añadimos puntos
-        if(pair.bodyA.label == 'porteria'){
-            if(ultimoGolpe!=-1) {
-                if(pair.bodyA.jugador==ultimoGolpe) {
-                    puntuaciones[ultimoGolpe]-= puntuaciones[ultimoGolpe]>0 ? 1:0;
+        if (pair.bodyA.label == 'porteria') {
+            if (ultimoGolpe != -1) {
+                if (pair.bodyA.jugador == ultimoGolpe) {
+                    puntuaciones[ultimoGolpe] -= puntuaciones[ultimoGolpe] > 0 ? 1 : 0;
                 } else {
                     puntuaciones[ultimoGolpe]++;
                 }
@@ -265,31 +265,31 @@ Events.on(engine, 'collisionStart', event => {
                 actualizarMarcadores();
             }
 
-            
-            if(pair.bodyA.label == 'Pelota') {
+
+            if (pair.bodyA.label == 'Pelota') {
                 Body.setPosition(pair.bodyA, Vector.create(PELOTA_POS_INI[0], PELOTA_POS_INI[1]));
                 velTot = PELOTA_MIN_VEL;
-                
-                if(idJugador==1) {
+
+                if (idJugador == 1) {
                     let newVel = getRandomVelocity(velTot);
 
                     socket.emit('puntua', newVel, puntuaciones);
-                    Body.setVelocity( bola, Vector.create(newVel.x, newVel.y) );
+                    Body.setVelocity(bola, Vector.create(newVel.x, newVel.y));
                 }
             } else {
                 Body.setPosition(pair.bodyB, Vector.create(PELOTA_POS_INI[0], PELOTA_POS_INI[1]));
                 velTot = PELOTA_MIN_VEL;
-                
+
                 //setRandVel(bola, velTot);
 
-                if(idJugador==1) {
+                if (idJugador == 1) {
                     let newVel = getRandomVelocity(velTot);
 
                     socket.emit('puntua', newVel, puntuaciones);
-                    Body.setVelocity( bola, Vector.create(newVel.x, newVel.y) );
+                    Body.setVelocity(bola, Vector.create(newVel.x, newVel.y));
                 }
             }
-            
+
         }
     }
 });
@@ -301,10 +301,10 @@ Events.on(engine, 'collisionEnd', event => {
         let pair = pairs[i];
 
         // Si la pelota choca con una paleta se incrementa su velocidad
-        if( (pair.bodyB.label.includes('paleta') ) && velTot <= PELOTA_MAX_VEL ) {
+        if ((pair.bodyB.label.includes('paleta')) && velTot <= PELOTA_MAX_VEL) {
             let labelLength = pair.bodyB.label.length;
 
-            switch(pair.bodyB.label.substring(labelLength-1, labelLength)) {
+            switch (pair.bodyB.label.substring(labelLength - 1, labelLength)) {
                 case "1":
                     ultimoGolpe = 0;
                     break;
@@ -316,28 +316,28 @@ Events.on(engine, 'collisionEnd', event => {
                     break;
             }
 
-            velTot+=0.5;
+            velTot += 0.5;
         }
 
         // Ajustamos la velocidad de la pelota para que no reducca la velocidad
-        if(idJugador==1){
-            if(pair.bodyA.label == 'Pelota'){
+        if (idJugador == 1) {
+            if (pair.bodyA.label == 'Pelota') {
                 let newVel = {
-                    x:pair.bodyA.velocity.x,
-                    y:( pair.bodyA.velocity.y>0 ? 1:-1 )*Math.sqrt(velTot**2-pair.bodyA.velocity.x**2)
+                    x: pair.bodyA.velocity.x,
+                    y: (pair.bodyA.velocity.y > 0 ? 1 : -1) * Math.sqrt(velTot ** 2 - pair.bodyA.velocity.x ** 2)
                 }
 
-                socket.emit('colision', newVel, { x:pair.bodyA.position.x, y:pair.bodyA.position.y });
-                Body.setVelocity(pair.bodyA, Matter.Vector.create( newVel.x, newVel.y ));
+                socket.emit('colision', newVel, { x: pair.bodyA.position.x, y: pair.bodyA.position.y });
+                Body.setVelocity(pair.bodyA, Matter.Vector.create(newVel.x, newVel.y));
 
             } else {
                 let newVel = {
-                    x:pair.bodyB.velocity.x,
-                    y:( pair.bodyB.velocity.y>0 ? 1:-1 )*Math.sqrt(velTot**2-pair.bodyB.velocity.x**2)
+                    x: pair.bodyB.velocity.x,
+                    y: (pair.bodyB.velocity.y > 0 ? 1 : -1) * Math.sqrt(velTot ** 2 - pair.bodyB.velocity.x ** 2)
                 }
 
-                socket.emit('colision', newVel, { x:pair.bodyB.position.x, y:pair.bodyB.position.y });
-                Body.setVelocity(pair.bodyB, Matter.Vector.create( newVel.x, newVel.y ));
+                socket.emit('colision', newVel, { x: pair.bodyB.position.x, y: pair.bodyB.position.y });
+                Body.setVelocity(pair.bodyB, Matter.Vector.create(newVel.x, newVel.y));
             }
         }
 
@@ -349,36 +349,36 @@ Events.on(engine, 'collisionEnd', event => {
     }
 });
 
-let itera=0;
+let itera = 0;
 
 Events.on(engine, 'beforeUpdate', event => {
     itera++;
-    
+
 
     // Controlamos que la paleta no se pase de los limites
 
-    if(idJugador!=0 && itera==4){
+    if (idJugador != 0 && itera == 4) {
         itera = 0;
 
-        if(idJugador==1)
+        if (idJugador == 1)
             socket.emit(
-                'colision', 
+                'colision',
                 bola.velocity,
                 bola.position
             );
-        
-        if(paletas[idJugador-1].moving != 0)
-            socket.emit('sync-pos-paleta', idJugador, paletas[idJugador-1].position);
+
+        if (paletas[idJugador - 1].moving != 0)
+            socket.emit('sync-pos-paleta', idJugador, paletas[idJugador - 1].position);
     }
 
-    if( (paleta.position.x>=443 || paleta.moving==1) && (paleta.position.x<=656 || paleta.moving==-1) ) 
-        Body.setPosition(paleta, Vector.add(paleta.position, Vector.create(PLANK_VEL*paleta.moving,0)));
+    if ((paleta.position.x >= 443 || paleta.moving == 1) && (paleta.position.x <= 656 || paleta.moving == -1))
+        Body.setPosition(paleta, Vector.add(paleta.position, Vector.create(PLANK_VEL * paleta.moving, 0)));
 
-    if( (paleta2.position.x>=789 || paleta2.moving==-1) && (paleta2.position.x<=893 || paleta2.moving==1) )
-        Body.setPosition( paleta2, moveAngle(paleta2.position, PLANK_VEL*paleta2.moving, -0.52) );
+    if ((paleta2.position.x >= 789 || paleta2.moving == -1) && (paleta2.position.x <= 893 || paleta2.moving == 1))
+        Body.setPosition(paleta2, moveAngle(paleta2.position, PLANK_VEL * paleta2.moving, -0.52));
 
-    if( (paleta3.position.x>=209 || paleta3.moving==-1) && (paleta3.position.x<=319 || paleta3.moving==1) )
-        Body.setPosition( paleta3, moveAngle(paleta3.position, PLANK_VEL*paleta3.moving, -2.6) );
+    if ((paleta3.position.x >= 209 || paleta3.moving == -1) && (paleta3.position.x <= 319 || paleta3.moving == 1))
+        Body.setPosition(paleta3, moveAngle(paleta3.position, PLANK_VEL * paleta3.moving, -2.6));
 
     lastUpdate = engine.timing.timestamp;
 });
@@ -390,20 +390,20 @@ Events.on(engine, 'beforeUpdate', event => {
  * @return {Vector} Retorna la posicion resultante del movimiento
  */
 function setRandVel(obj, targetVel) {
-    let velX = Math.random()*(targetVel*2+1)-targetVel;
+    let velX = Math.random() * (targetVel * 2 + 1) - targetVel;
     Body.setVelocity(obj, Vector.create(
         velX,
-        ((Math.random()>0.5)?1:-1)*Math.sqrt(targetVel**2-velX**2)
+        ((Math.random() > 0.5) ? 1 : -1) * Math.sqrt(targetVel ** 2 - velX ** 2)
     ));
 }
 
 function getRandomVelocity(targetVel) {
-    let velX = Math.random()*(targetVel*2+1)-targetVel;
+    let velX = Math.random() * (targetVel * 2 + 1) - targetVel;
     let vResult = {
-            x: velX,
-            y: (Math.random() > 0.5 ? 1:-1)*Math.sqrt(targetVel**2-velX**2)
+        x: velX,
+        y: (Math.random() > 0.5 ? 1 : -1) * Math.sqrt(targetVel ** 2 - velX ** 2)
     };
-    
+
     return vResult;
 }
 
@@ -415,7 +415,7 @@ function getRandomVelocity(targetVel) {
  * @return {Vector} Retorna la posicion resultante del movimiento
  */
 function moveAngle(position, vel, angle) {
-    return Vector.add( Vector.rotate( Vector.create(0,-vel), angle ), position );
+    return Vector.add(Vector.rotate(Vector.create(0, -vel), angle), position);
 }
 
 function actualizarMarcadores() {
@@ -436,12 +436,12 @@ CustomRender.run(render);
 
 // Socket.io
 socket.on('succes-conn', id => {
-    if(idJugador == 0) {
+    if (idJugador == 0) {
         idJugador = id;
         console.log(`Id: ${idJugador}`);
 
         // --- Asignamos las teclas de movimiento segun el id ---
-        switch(idJugador) {
+        switch (idJugador) {
             case 1:
                 keyAsignation(keys, paleta, idJugador);
                 break;
@@ -455,19 +455,19 @@ socket.on('succes-conn', id => {
     }
 });
 
-socket.on('sync-call', (newVel, newPos) => {    
-    Body.setPosition( bola, 
-        Matter.Vector.create( 
-            newPos.x, 
+socket.on('sync-call', (newVel, newPos) => {
+    Body.setPosition(bola,
+        Matter.Vector.create(
+            newPos.x,
             newPos.y
-        ) 
+        )
     );
 
-    Body.setVelocity( bola, Matter.Vector.create( newVel.x, newVel.y ) );
+    Body.setVelocity(bola, Matter.Vector.create(newVel.x, newVel.y));
 });
 
 socket.on('move-paleta', (dir, id) => {
-    switch(id) {
+    switch (id) {
         case 1:
             paleta.moving = dir;
             break;
@@ -481,7 +481,7 @@ socket.on('move-paleta', (dir, id) => {
 });
 
 socket.on('sync-paleta', (dir, id, position) => {
-    switch(id) {
+    switch (id) {
         case 1:
             paleta.moving = dir;
             Body.setPosition(paleta, position);
@@ -497,15 +497,15 @@ socket.on('sync-paleta', (dir, id, position) => {
     }
 });
 
-socket.on('sync-pos-paleta', (id, position) => Body.setPosition(paletas[id-1],position) );
+socket.on('sync-pos-paleta', (id, position) => Body.setPosition(paletas[id - 1], position));
 
 socket.on('full', v => {
-    Body.setVelocity( bola, Vector.create(v.x, v.y) );
+    Body.setVelocity(bola, Vector.create(v.x, v.y));
     console.log(`Vel set to: x-${v.x} y-${v.y} | Bola vel: x-${bola.velocity.x} y-${bola.velocity.y}`);
 });
 
 socket.on('puntua', (v, puntuacionesSync) => {
-    Body.setVelocity( bola, Vector.create(v.x, v.y) );
+    Body.setVelocity(bola, Vector.create(v.x, v.y));
 
     puntuaciones[0] = puntuacionesSync[0];
     puntuaciones[1] = puntuacionesSync[1];
@@ -514,12 +514,12 @@ socket.on('puntua', (v, puntuacionesSync) => {
     actualizarMarcadores();
 });
 
-socket.on('user-disconnect', idDisconn=>{
+socket.on('user-disconnect', idDisconn => {
     console.log(`Player id: ${idDisconn} disconected`);
-    if(idDisconn == 1) {
+    if (idDisconn == 1) {
         idJugador--;
         socket.emit('id-update', idJugador);
-    } else if(idDisconn == 2 && idJugador == 3) {
+    } else if (idDisconn == 2 && idJugador == 3) {
         idJugador--;
         socket.emit('id-update', idJugador);
     }
@@ -528,7 +528,7 @@ socket.on('user-disconnect', idDisconn=>{
 });
 
 socket.on('cambio-usuario', (data, id) => {
-    if(id===idJugador) document.getElementById('login').classList.toggle('oculta');
+    if (id === idJugador) document.getElementById('login').classList.toggle('oculta');
 
     puntuacionJ1.jugador = data[0].name;
     puntuacionJ2.jugador = data[1].name;
@@ -550,3 +550,10 @@ socket.on('cambio-usuario', (data, id) => {
         size: 20
     }
 });
+
+/*window.addEventListener('resize', reportWindowSize);
+
+function reportWindowSize() {
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+}*/
